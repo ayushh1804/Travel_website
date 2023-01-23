cf48995c0d86682802f8055408367465aaac0ec1
AYUSH <ayushrajbls@gmail.com>
2023-01-23 23:45:06 +0530

    Added input fields for the card

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import video from "../../assets/Blue.mp4"
+import React from "react";
+import video from "../../assets/Blue.mp4";
+import "./home.css";
+import { SlLocationPin } from "react-icons/sl";
+
 const Home = () => {
   return (
     <section className="home">
       <div className="overlay"></div>
-    <video src={video} muted autoPlay loop type="video/mp4" height='300'></video>
-    </section>
-  )
-}
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
 
-export default Home
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">Our Packages</span>
+          <h1 className="homeTitle">Search your Holidays</h1>
+        </div>
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search your Destination</label>
+            <div className="input flex">
+              <input type="text" placeholder="Enter Name Here...." />
+              <SlLocationPin className="icon" color="black" />
+            </div>
+          </div>
+          <div className="dateInput">
+            <label htmlFor="date">Enter the date</label>
+            <div className="input flex">
+              <input type="date" />
+              
+            </div>
+          </div>
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">Max Price </label>
+              <h3 className="total">$500</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="500"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
